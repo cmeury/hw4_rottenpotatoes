11926b8e47f0ffed59439948fb96e44735b45a74
Cedric Meury <cedric@meury.com>
2013-02-13 15:13:43 -0800

    scenario 1 passes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+
+  def self.find_by_title (title)
+    return Movie.where("title = '#{title}'").first
+  end
 end
